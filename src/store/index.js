708f0c24cb9dced5370251634f5ea0cfca980915
大叔阿哲 <2033244875@qq.com>
2024-04-02 14:05:30 +0800

    gagag

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 大叔阿哲 2033244875@qq.com
+ * @Date: 2024-03-19 17:12:46
+ * @LastEditors: 大叔阿哲 2033244875@qq.com
+ * @LastEditTime: 2024-03-30 12:14:07
+ * @FilePath: \ticket-vue\src\store\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /* eslint-disable */
 
 import Vue from 'vue'
